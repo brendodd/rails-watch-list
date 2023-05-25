c50d105cbbd3286f987f9660efdd435a0db46a21
Brendan Dodd <brendananthonydodd@gmail.com>
2023-05-25 13:07:48 +0200

    validates the three models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :overview, presence: true
+  validates :title, uniqueness: { case_sensitive: false }
+  validates :title, presence: true
 end
